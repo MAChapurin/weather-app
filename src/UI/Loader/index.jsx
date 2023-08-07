cefd2f689e37fc360a25250b623bebd96a3a232c
Michael <machapurin@icloud.com>
2023-08-07 10:58:42 +0300

    Updates

--- src/UI/Loader/index.jsx
+++
@@ -1,7 +0,0 @@
-import styles from './Loader.module.css'
-
-export function Loader() {
-  return (
-    <div className={styles['lds-ellipsis']}><div></div><div></div><div></div><div></div></div>
-  )
-}
--- src/context/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export const ThemeContext = React.createContext(null);
-
-export const CityContext = React.createContext(null);
-
-export const DetailContext = React.createContext(null);
